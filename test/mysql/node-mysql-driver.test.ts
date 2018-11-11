86cefd91b5ee57ad5bb5cb15b08d861508e575f9
jaydee <jindongp@gmail.com>
2018-11-11 16:41:54 +0900

    added more implementation & tests for node-mysql-driver

--- test/mysql/node-mysql-driver.test.ts
+++ test/mysql/node-mysql-driver.test.ts
@@ -1,7 +1,9 @@
 import { 
   NativePool,
   createPoolConnectionDriver,
-  MysqlInitError } from '../../src/mysql';
+  MysqlInitError, 
+  MysqlQueryError} from '../../src/mysql';
+import { createPoolConnectionTester } from '../../src/mysql/node-mysql-driver';
 
 describe('node-mysql-driver tests', () => {
   test('if getConnection returns error, must be throw error', () => {
@@ -13,6 +15,41 @@ describe('node-mysql-driver tests', () => {
   });
 
   test('if getConnection() returns Connection, and if error occurs during query(), must be throw error', () => {
-    
+    const mockPool: NativePool = {
+      getConnection: (callback) => callback(null, {
+        query(query, params, callback) {
+          callback(new Error('test-query-error'), null);
+        },
+        release(callback) {}
+      })
+    }
+    createPoolConnectionDriver(mockPool)
+    .then((driver) => {
+      expect(driver.query('test-query', [])).rejects.toBeInstanceOf(MysqlQueryError);
+    });
+  });
+});
+
+describe('PoolConnectionTester tests', () => {
+  test('if nativePool throws error, poolTester must be throw error', () => {
+    const mockPool: NativePool = {
+      getConnection: (callback) => callback(new Error('test-error'), null)
+    };
+    const tester = createPoolConnectionTester(mockPool);
+    expect(tester()).rejects.toBeInstanceOf(MysqlInitError);
+  });
+
+  test('if nativePool returns connection normally, con::release() must be called', () => {
+    const mockRelease = jest.fn((callback) => {});
+    const mockPool: NativePool = {
+      getConnection: (callback) => callback(null, {
+        query(query, params, callback) {},
+        release: mockRelease
+      })
+    };
+    const tester = createPoolConnectionTester(mockPool);
+    tester().then(() => {
+      expect(mockRelease).toBeCalledTimes(1);
+    });
   });
 });
