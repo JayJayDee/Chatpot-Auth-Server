92a0f997ae5badb1e5a893105358d8040f51994a
JayJayDee <jindongp@gmail.com>
2019-03-23 22:51:38 +0900

    completed POST /member/email api.

--- src/new-endpoints/member-endpoints.ts
+++ src/new-endpoints/member-endpoints.ts
@@ -1,6 +1,8 @@
 import { injectable } from 'smart-factory';
 import { EndpointModules } from './modules';
 import { EndpointTypes } from './types';
+import { ServiceModules, ServiceTypes } from '../services';
+import { InvalidParamError } from '../errors';
 
 injectable(EndpointModules.Member.CreateSimple,
   [ EndpointModules.Utils.WrapAync ],
@@ -18,15 +20,35 @@ injectable(EndpointModules.Member.CreateSimple,
 
 
 injectable(EndpointModules.Member.CreateEmail,
-  [ EndpointModules.Utils.WrapAync ],
-  async (wrapAsync: EndpointTypes.Utils.WrapAsync): Promise<EndpointTypes.Endpoint> =>
+  [ EndpointModules.Utils.WrapAync,
+    ServiceModules.Member.Create ],
+  async (wrapAsync: EndpointTypes.Utils.WrapAsync,
+    create: ServiceTypes.CreateMember): Promise<EndpointTypes.Endpoint> =>
 
   ({
     uri: '/member/email',
     method: EndpointTypes.EndpointMethod.POST,
     handler: [
-      wrapAsync((req, res, next) => {
-        res.status(200).json({});
+      wrapAsync(async (req, res, next) => {
+        const email = req.body['email'];
+        const password = req.body['password'];
+        const region = req.body['region'];
+        const language = req.body['language'];
+        const gender = req.body['gender'];
+
+        if (!email || !password || !region || !language || !gender) {
+          throw new InvalidParamError('email, password, region, language, gender required');
+        }
+
+        const auth = {
+          auth_type: ServiceTypes.AuthType.EMAIL,
+          login_id: email,
+          password
+        };
+
+        const param = { region, language, gender, auth };
+        const resp = await create(param);
+        res.status(200).json(resp);
       })
     ]
   }));
