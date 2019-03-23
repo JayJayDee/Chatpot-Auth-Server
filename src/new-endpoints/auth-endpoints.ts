09059a4afd7dcf66fd69537e96d88181bd5a2a8a
JayJayDee <jindongp@gmail.com>
2019-03-24 00:28:24 +0900

    completed POST /auth endpoint.

--- src/new-endpoints/auth-endpoints.ts
+++ src/new-endpoints/auth-endpoints.ts
@@ -29,15 +29,23 @@ injectable(EndpointModules.Auth.AuthEmail,
 
 
 injectable(EndpointModules.Auth.AuthSimple,
-  [ EndpointModules.Utils.WrapAync ],
-  async (wrapAsync: EndpointTypes.Utils.WrapAsync): Promise<EndpointTypes.Endpoint> =>
+  [ EndpointModules.Utils.WrapAync,
+    ServiceModules.Member.Authenticate ],
+  async (wrapAsync: EndpointTypes.Utils.WrapAsync,
+    authenticate: ServiceTypes.Authenticate): Promise<EndpointTypes.Endpoint> =>
 
   ({
     uri: '/auth',
     method: EndpointTypes.EndpointMethod.POST,
     handler: [
-      wrapAsync((req, res, next) => {
-        res.status(200).json({});
+      wrapAsync(async (req, res, next) => {
+        const login_id = req.body['login_id'];
+          const password = req.body['password'];
+          if (!login_id || !password) throw new InvalidParamError('login_id, password');
+          const auth_type = ServiceTypes.AuthType.SIMPLE;
+
+          const resp = await authenticate({ login_id, password, auth_type });
+          res.status(200).json(resp);
       })
     ]
   }));
