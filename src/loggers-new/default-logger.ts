275bd0dca0b7f95de9b509d63dabcd7b352a8fba
JayJayDee <jindongp@gmail.com>
2019-03-24 13:49:00 +0900

    refactoring logger sturucture.

---
+++ src/loggers-new/default-logger.ts
@@ -0,0 +1,23 @@
+import { injectable } from 'smart-factory';
+import { LoggerModules } from './modules';
+import * as moment from 'moment';
+
+import { Modules } from '../modules';
+import { Env } from '../config/types';
+import { Logger } from '../loggers/types';
+
+injectable(LoggerModules.Logger,
+  [ Modules.Config.Env ],
+  async (env: Env): Promise<Logger> => ({
+    info(payload: any) {
+      console.log(prefix(), payload);
+    },
+    debug(payload: any) {
+      if (env === Env.DEV) console.log(prefix(), payload);
+    },
+    error(payload: any) {
+      console.error(prefix(), payload);
+    }
+  }));
+
+const prefix = (): string => `[${moment().format('YYYY-MM-DD HH:mm:ss')}]`;
