15a78c54b51fba115ec53b1167dca41f6382e2df
JayJayDee <jindongp@gmail.com>
2019-05-20 12:40:14 +0900

    added email-auth creation query.

--- src/stores/email-auth-store.ts
+++ src/stores/email-auth-store.ts
@@ -1,8 +1,16 @@
 import { injectable } from 'smart-factory';
+import { createHash } from 'crypto';
 import { StoreModules } from './modules';
 import { LoggerModules, LoggerTypes } from '../loggers';
 import { StoreTypes } from './types';
 import { MysqlModules, MysqlTypes } from '../mysql';
+
+
+const generateCode = (memberNo: number, email: string) =>
+  createHash('sha1')
+    .update(`${memberNo}${email}${Date.now()}`)
+    .digest('hex');
+
 
 injectable(StoreModules.Member.CreateEmailAuth,
   [ LoggerModules.Logger,
@@ -10,8 +18,34 @@ injectable(StoreModules.Member.CreateEmailAuth,
   async (log: LoggerTypes.Logger,
     mysql: MysqlTypes.MysqlDriver): Promise<StoreTypes.Member.CreateEmailAuth> =>
 
-      async (param) => {
+    async (param) => {
+      mysql.transaction(async (con) => {
+        try {
+          const sql = `
+            INSERT INTO
+              chatpot_email
+            SET
+              member_no=?,
+              email=?,
+              state='SENT',
+              code=?,
+              reg_date=NOW()
+          `;
+          const code = generateCode(param.member_no, param.email);
+          const params = [
+            param.member_no,
+            param.email,
+            code
+          ];
+          await con.query(sql, params);
+
+        } catch (err) {
+          log.error(err);
+          con.rollback();
+          throw err;
+        }
       });
+    });
 
 
 injectable(StoreModules.Member.VerifyEmailAuthCompleted,
