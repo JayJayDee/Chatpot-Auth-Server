a97680699ec2a2da353022a0b7cd936fcc3c2993
JayJayDee <jindongp@gmail.com>
2019-05-21 14:34:59 +0900

    added email contents.

--- src/mailer/types.ts
+++ src/mailer/types.ts
@@ -1,3 +1,7 @@
 export namespace MailerTypes {
-  export type SendActivationMail = (email: string) => Promise<void>;
+  type ActivationParam = {
+    email: string;
+    code: string;
+  };
+  export type SendActivationMail = (param: ActivationParam) => Promise<void>;
 }
