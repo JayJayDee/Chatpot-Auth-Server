c494a3571d8262773cd7060daccae104481eed1d
JayJayDee <jindongp@gmail.com>
2019-05-21 23:26:31 +0900

    added activation-store.

---
+++ src/stores/activation-store.ts
@@ -0,0 +1,27 @@
+import { injectable } from 'smart-factory';
+import { StoreModules } from './modules';
+import { LoggerModules, LoggerTypes } from '../loggers';
+import { MysqlModules, MysqlTypes } from '../mysql';
+import { StoreTypes } from './types';
+
+
+injectable(StoreModules.Activation.GetActivationStatus,
+  [ LoggerModules.Logger,
+    MysqlModules.MysqlDriver ],
+  async (log: LoggerTypes.Logger,
+    mysql: MysqlTypes.MysqlDriver): Promise<StoreTypes.Activation.GetActivationStatus> =>
+
+    async (param) => {
+
+    });
+
+
+injectable(StoreModules.Activation.Activate,
+  [ LoggerModules.Logger,
+    MysqlModules.MysqlDriver ],
+  async (log: LoggerTypes.Logger,
+    mysql: MysqlTypes.MysqlDriver): Promise<StoreTypes.Activation.Activate> =>
+
+    async (param) => {
+
+    });
